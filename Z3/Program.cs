5188fb02c7c60dc3a24c7f9b777f02d496951468
Mikhail Dementev <slovetskiyoriginal@gmail.com>
2022-08-11 22:38:45 +0300

    task3

---
+++ Z3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("Это выходной день");
+    }
+    else if (dayNumber < 1 || dayNumber > 7)
+    {
+        Console.WriteLine("это вообще не день недели");
+    }
+    else Console.WriteLine("Это не выходной день");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
